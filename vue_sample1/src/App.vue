c9cc6980a760bd6ed306cc1bafa3610f42414862
zhulh <1053831559@qq.com>
2022-03-26 21:38:27 +0800

    chore: 初始化

---
+++ vue_sample1/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div div="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+export default {
+  components: { Hello },
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
